1f5aef32414136e65010e2745d900dac12afce20
faviocpz <103940536+faviocpz@users.noreply.github.com>
2024-11-22 02:17:22 -0500

    NUEVO HOST

--- bd.py
+++ bd.py
@@ -4,14 +4,14 @@ def obtener_conexion():
     try:
         conexion = pymysql.connect(
             host='junction.proxy.rlwy.net',
-            port=39981,
+            port=28269,
             user='root',
-            password='pCWZPaxqsNKSYInhPHeyFknIWiRFpSpb',
+            password='dvIcHQBFZrICrloweOguVmZkHcoVluEA',
             db='bd_ppp'
         )
         print("Conexión exitosa a la base de datos.")
         return conexion
-    #junction.proxy.rlwy.net:39981
+    #junction.proxy.rlwy.net:28269
     except Exception as e:
         print(f"Error al conectar a la base de datos: {e}")
         return None 
